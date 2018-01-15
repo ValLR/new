ea64291280a003adec2c2633f6b0d317ea8862fe
ValLR <valentinalopezrecabal@gmail.com>
2018-01-15 18:54:29 -0300

    cambios en lógica mapeo

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,10 +4,10 @@ import {
   graphql
 } from 'react-relay'
 
+
 const available_services = ['pivotal', 'github', 'codeship', 'papertrail', 'sentry']
 
 class Project extends Component {
-
 
   render() {
     return (
@@ -16,54 +16,95 @@ class Project extends Component {
           <h3 key={this.props.project.id}>{this.props.project.canonicalName}</h3>
           {available_services.map((service) =>
             <div>
-              <Service service={service}/>
+              <h4>{service}</h4>
               <div>
-                {this.props.project.resources.edges.map(({node}) =>
-                  <p>{node.type !== service
-                    ? "node.type"
-                    : service}
-                  </p>  
-                )}
+                  <Icon service={service} project={this.props.project}/>
               </div>
             </div>
           )}
         </div>
-      <div>
-        <div>
-          <h4>Pivotal</h4>
-          <p>yes/no</p>
-        </div>
-        <div>
-          <h4>Github</h4>
-          <p>yes/no</p>
-        </div>
-        <div>
-          <h4>Sentry</h4>
-          <p>yes/no</p>
-        </div>
-        <div>
-          <h4>Codeship</h4>
-          <p>yes/no</p>
-        </div>
-        <div>
-          <h4>Favicon</h4>
-          <p>yes/no</p>
-        </div>
       </div>
-    </div>
+    )
+  }
+}
+
+class Icon extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      hasService: false,
+      url:""
+    }
+  }
+
+  componentDidMount(){
+    this.props.project.resources.edges.map(({node}) =>
+      node.type === this.props.service ?
+        this.setState({
+          hasService:true,
+          url:node.url
+        })
+      :
+      ''
+    )
+
+  }
+
+  render() {
+    return (
+      <div>
+        <Logo url={this.state.url} state={this.state.hasService} service={this.props.service} />
+      </div>
     )
   }
 }
 
 
-class Service extends Component {
-  render(){
-    return(
-      <h4>{this.props.service}</h4>
+class Logo extends Component {
+
+  render() {
+
+    const service = this.props.service;
+    const initialPath = "/images/";
+    const finalPath = ".png";
+    const source = initialPath + service + finalPath;
+    
+    return (
+      <div>
+        {this.props.state ?
+          <div>
+            <p>YES, has access to {this.props.service}</p>
+            <a href={this.props.url}><img src={source} class="access" /></a>
+          </div>    
+        :
+          <div>
+            <p>NO, no access to {this.props.service}</p>
+            <a href="#" class="disabled"><img src={source} class="disabled" /></a>
+          </div>
+        }
+      </div>
     )
   }
 }
+/*
 
+  <IsEmpty connectedService={node.type} availableService />
+
+  class IsEmpty extends Component {
+
+    constructor(props){
+      super(props);
+      this.state = {
+        service:""
+      }
+    }
+
+    componentDidMount(){
+      this.setS
+    }
+  }
+*/
 
 export default createFragmentContainer(Project, graphql`
   fragment Project_project on Project {
